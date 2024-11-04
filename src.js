7baf34b9421e5fa5499c7bf1215f235b55894821
Wizqk <leskom92@gmail.com>
2024-11-04 16:52:11 +0100

    initial calc logic

--- src.js
+++ src.js
@@ -9,7 +9,9 @@ for (i = 1; i < 14; i++) {
   } else {
     const button = document.createElement("button");
     button.style.cssText = "height: 60; width: 55;";
-    buttonNumber === 10 ? (button.textContent = 0) : (button.textContent = buttonNumber);
+    buttonNumber === 10
+      ? (button.textContent = 0)
+      : (button.textContent = buttonNumber);
     buttonNumber++;
     numpad.appendChild(button);
   }
@@ -36,20 +38,58 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  let result;
   switch (operator) {
-    case "add":
-      add(num1, num2);
-      break;
-    case "subtract":
-      subtract(num1, num2);
-      break;
-    case "multiply":
-      multiply(num1, num2);
-      break;
-    case "divide":
-      divide(num1, num2);
-      break;
+    case "+":
+      result = add(num1, num2);
+      return result;
+    case "-":
+      result = subtract(num1, num2);
+      return result;
+    case "*":
+      result = multiply(num1, num2);
+      return result;
+    case "/":
+      result = divide(num1, num2);
+      return result;
     default:
       throw new Error("What is this?");
   }
 }
+
+const numButtons = document.querySelectorAll(".calc-numpad button");
+const display = document.querySelector("input");
+const optButtons = document.querySelectorAll(".calc-operators button");
+
+numButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let enteredValue = display.value;
+    display.value = enteredValue + button.textContent;
+  });
+});
+
+optButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "=") {
+    } else {
+      operation = button.textContent;
+    }
+    if (operation === "c") {
+      firstNum = undefined;
+      secondNum = undefined;
+      display.value = "";
+    }
+    if (firstNum === undefined && display.value) {
+      firstNum = display.value;
+      display.value = "";
+    } else {
+      secondNum = display.value;
+    }
+    if (operation && firstNum && secondNum) {
+      const result = operate(operation, firstNum, secondNum);
+      display.value = result;
+    }
+  });
+});
